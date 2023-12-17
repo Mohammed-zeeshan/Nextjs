cec5e9c288923eb8128a02a59b919d553ec278c8
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-18 02:26:37 +0530

    Final touch to About US page

--- about.js
+++ about.js
@@ -1,7 +1,17 @@
-// our-domain.com/about
+// our-domain.com/aboutus
+
+import { Fragment } from "react";
 
 function AboutPage() {
-    return <h1>The Home Page</h1>
+    return (
+        <Fragment>
+            <ul>
+                <li><Link href='aboutus/1'>Yash</Link></li>
+                <li><Link href='aboutus/2'>Vaibhav</Link></li>
+                <li><Link href='aboutus/2'>Suresh</Link></li>
+            </ul>
+        </Fragment>
+    )
 }
 
 export default AboutPage;
