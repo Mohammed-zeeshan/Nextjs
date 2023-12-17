779fc7ac3dcb4a1c1c736aa4446524353ae4f5f3
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-18 02:15:30 +0530

    Dynamic page routes

--- developer.js
+++ developer.js
@@ -1,7 +1,23 @@
 // our-domain.com/aboutus/developer
 
+import { useRouter } from "next/router";
+
 function DeveloperPage() {
-    return <h1>zeeshan</h1>
+    const router = useRouter()
+
+    let details = [
+
+        { id : 1, name: 'Yash', role: 'Senior Developer'},
+        
+        { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+        
+        { id : 3, name: 'Suresh', role: 'Frontend Developer'},
+        
+    ]
+
+    let index = Number(router.query.aboutus) - 1;
+
+    return <label>{details[index].name} {details[index].role}</label>
 }
 
 export default DeveloperPage;
