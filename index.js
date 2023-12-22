45ebf32dd428dc4698e4a1193dde18d7a9e67ae1
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-22 19:41:08 +0530

    Time to save the TODOs

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 import { Fragment } from "react";
 import TodoListForm from "../Components/TodoListForm";
+import List from "../Components/Lists";
+import { MongoClient } from "mongodb";
 
-function HomePage() {
+function HomePage(props) {
+  async function addItemsHandler(enteredItems) {
+    const response = await fetch("/api/new-items", {
+      method: "POST",
+      body: JSON.stringify(enteredItems),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+  console.log(props.todolist);
   return (
     <Fragment>
-      <TodoListForm />
+      <TodoListForm onAddItems={addItemsHandler} />
+      <List todolist={props.todolist} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://mohammedzeeshan440:vxFaEVGFTlab3hZW@cluster0.fffqgk6.mongodb.net/todo?retryWrites=true&w=majorityy"
+  );
+  const db = client.db();
+
+  const todocollections = db.collection("todolist");
+
+  const todolist = await todocollections.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      todolist: todolist.map((item) => ({
+          todo: item.todo,
+          id: item._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
 }
 
 export default HomePage;
